f54e43d0d376afcaad0b498b0588f2a944c809fd
Mason Hoffman <mhoff873@users.noreply.github.com>
2017-10-02 23:46:20 -0400

    Add files via upload

--- connect - template.php
+++ connect - template.php
@@ -1,5 +1,4 @@
-<?php
-        #You need to fill in log-in information for the database server.
+<?php  
 	function db_connect1(){
 		$db_host = '';
 		$db_user= '';
@@ -16,23 +15,4 @@
 		global $connection;
 		mysql_close($connection);
 	}
-	db_connect1(); //CONNECT TO DATABASE RIGHT AWAY
-
-	$getDetails="SELECT * FROM `table 1`"; 
-	
-	$fill = mysql_query($getDetails);
-
- 	$data=array();
-        for ($x = 0; $x < mysql_num_rows($fill); $x++){
-            $row = mysql_fetch_assoc($fill);  
-            array_push($data,array(
-		"name"=> mysql_result($fill,$x,"name"),
-		"tax" => mysql_result($fill,$x,"tax"),
-		"lat"=> mysql_result($fill,$x,"lat"),
-		"lon"=>mysql_result($fill,$x,"lon")));
-        } 
-        
-	//PHP GLOBALS	
-	echo "<script> var data = ".json_encode($data).";\n";
-        echo "</script>";
 ?>
