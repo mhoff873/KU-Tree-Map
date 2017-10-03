5067925c9446c67ce357a9abf5c0d46f98d6e174
Mason Hoffman <mhoff873@users.noreply.github.com>
2017-10-02 23:47:39 -0400

    Add files via upload

--- index - template.php
+++ index - template.php
@@ -2,9 +2,9 @@
 <head>
 <!--FULLSCREEN STYLE-->
 <link rel="stylesheet" type="text/css" href="style.css">
-
-<!--CONNECT TO DATABASE-->
-<?php include 'connect.php'; ?>
+  
+<!--PULL DATA FROM DATABASE-->
+<?php include 'pull.php'; ?>
 
 </head>
 <body>
@@ -16,10 +16,9 @@
 <script src="mapper.js" type="text/javascript"></script> 
 
 <!--CLOSE DATABASE-->
-<?php db_close1(); ?>
+<?php db_close1();?>
 
 <!--LOAD MAP TILES-->
-<!--THIS TEMPLATE REQUIRES YOU TO ENTER YOUR OWN GOOGLE MAPS API KEY -->
 <script src="https://maps.googleapis.com/maps/api/js?key=<-   ADD YOUR KEY HERE   ->&callback=initMap"
     async defer>
 </script>
